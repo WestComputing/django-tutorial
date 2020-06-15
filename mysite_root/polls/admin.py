4129fac1b97a8b798ffa2d8c667dd34d46cdf819
Julian West <west221b@gmail.com>
2020-06-15 17:50:38 -0400

    Add part 7, custom admin

--- mysite_root/polls/admin.py
+++ mysite_root/polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Date', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None, {'fields': ['question_text']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
